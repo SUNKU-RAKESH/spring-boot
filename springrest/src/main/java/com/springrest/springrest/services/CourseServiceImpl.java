17c10c7352a0dff2ae053c7545635da60abe0efe
SUNKU-RAKESH <66239151+SUNKU-RAKESH@users.noreply.github.com>
2023-04-09 21:34:49 +0530

    Add files via upload

---
+++ springrest/src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -0,0 +1,65 @@
+package com.springrest.springrest.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+import com.springrest.springrest.entities.Course;
+
+@Service
+public class CourseServiceImpl implements CourseService {
+	
+	
+	List<Course> list;
+	
+	public CourseServiceImpl() {
+		list = new ArrayList<>();
+		list.add(new Course(145, "Java Core Course", "This course contains basics of java"));
+		list.add(new Course(4343, "spring boot course", "creating rest api using spring boot"));
+	}
+
+	@Override
+	public List<Course> getCourses() {
+		// TODO Auto-generated method stub
+		return list;
+	}
+
+	@Override
+	public Course getCourse(long courseId) {
+		Course c = null;
+		for(Course course : list) {
+			if(course.getId()==courseId) {
+				c=course;
+				break;
+			}
+		}
+		return c;
+	}
+
+	@Override
+	public Course addCourse(Course course) {
+		list.add(course);
+		return course;
+	}
+
+	@Override
+	public Course updateCourse(Course course) {
+		list.forEach(e -> {
+			if(e.getId() == course.getId()) {
+				e.setTitle(course.getTitle());
+				e.setDescription(course.getDescription());
+			}
+		});
+		return course;
+	}
+
+	@Override
+	public void deleteCourse(long parseLong) {
+		list = this.list.stream().filter(e -> e.getId()!= parseLong).collect(Collectors.toList());
+		
+	}
+
+}
